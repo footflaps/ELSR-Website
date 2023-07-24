35f2a75c82ab20cfa5975bbba5f6389f7af8fabf
Ben Freeman <brf@cbnl.com>
2023-07-24 20:30:43 +0100

    fix for dual log entries

--- core/__init__.py
+++ core/__init__.py
@@ -83,6 +83,7 @@ gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.DEBUG)
 app.logger.debug('this will show in the log')
+app.logger.propagate = False
 
 
 # -------------------------------------------------------------------------------------------------------------- #
