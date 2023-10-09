0350385871ac46410533aab2d9bb5b1fcac8046c
Ben Freeman <brf@cbnl.com>
2023-10-09 14:37:32 +0100

    subs_google_maps.py: updated colour palette

--- core/subs_google_maps.py
+++ core/subs_google_maps.py
@@ -36,10 +36,10 @@ TRIM_DISTANCE_KM = 2.0
 
 # For displaying multiple GPX traces
 # based on https://loading.io/color/feature/Spectral-10/
-GPX_COLOURS = ["#9e0142", "#5e4fa2", "#d53e4f", "#3288bd", "#f46d43", "#66c2a5", "#fdae61", "#e6f598"]
-
+# GPX_COLOURS = ["#9e0142", "#5e4fa2", "#d53e4f", "#3288bd", "#f46d43", "#66c2a5", "#fdae61", "#e6f598"]
+GPX_COLOURS = ["#b30000", "#7c1158", "#4421af", "#1a53ff", "#0d88e6", "#00b7c7", "#5ad45a", "#8be04e", "#ebdc78"]
 # Don't display 100s on a map as total mess
-MAX_NUM_GPX_PER_GRAPH = 8
+MAX_NUM_GPX_PER_GRAPH = 9
 
 # EL coords
 ELSR_HOME = {"lat": 52.199234344363, "lng": 0.113774646436378}
