a6c1316cf8e910b98cb8ea4f6507247a36680437
Ben Freeman <brf@cbnl.com>
2023-11-12 18:38:24 +0000

    cafe_add.html: debug

--- core/templates/cafe_add.js
+++ core/templates/cafe_add.js
@@ -7,9 +7,7 @@ function locateMe() {
     // Can we get the user location?
     if (navigator.geolocation) {
 
-        alert("Before");
-        navigator.geolocation.getCurrentPosition(fillInForm);
-        alert("After");
+        navigator.geolocation.getCurrentPosition(fillInForm, showError);
 
     } else {
 
@@ -112,4 +110,21 @@ function fillInForm(position) {
     infoWindow.setOptions({ position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)});
     infoWindow.setContent("Form has been updated");
 
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
 }
