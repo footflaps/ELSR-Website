4e6127663b20f5f248544ede31e20a1907b20d0b
Ben Freeman <brf@cbnl.com>
2023-11-12 18:39:32 +0000

    cafe_add.html: debug

--- core/templates/cafe_add.js
+++ core/templates/cafe_add.js
@@ -115,16 +115,16 @@ function fillInForm(position) {
 function showError(error) {
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      x.innerHTML = "User denied the request for Geolocation."
+      alert("User denied the request for Geolocation.");
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "Location information is unavailable."
+      alert("Location information is unavailable.");
       break;
     case error.TIMEOUT:
-      x.innerHTML = "The request to get user location timed out."
+      alert("The request to get user location timed out.");
       break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML = "An unknown error occurred."
+      alert("An unknown error occurred.");
       break;
   }
 }
