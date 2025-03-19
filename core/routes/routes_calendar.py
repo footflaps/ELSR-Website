b0565aa67a4c4bdbba2973cdd50e88259f68eae2
Ben Freeman <brf@cbnl.com>
2025-03-19 19:01:53 +0000

    updated chaingang in calendar

--- core/routes/routes_calendar.py
+++ core/routes/routes_calendar.py
@@ -35,9 +35,9 @@ LOOK_FORWARD_MONTHS = 6
 # ELSR Chaingang details
 CHAINGANG_DAY = "Thursday"
 # From last week of March
-CHAINGANG_START_DATE = datetime(2024, 3, 28, 0, 00)
+CHAINGANG_START_DATE = datetime(2025, 4, 3, 0, 00)
 # To last week of September
-CHAINGANG_END_DATE = datetime(2024, 9, 19, 0, 00)
+CHAINGANG_END_DATE = datetime(2025, 9, 18, 0, 00)
 
 # Tim's Turbo Sessions (TTS) details
 TTS_DAY = "Tuesday"
